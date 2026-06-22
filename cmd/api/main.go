f7e7f995d290280d68abfc4b9cee6e6927165113
zhenghchen <zhenghchen2024@gmail.com>
2026-06-22 11:58:25 -0700

    added dynamodb for persistent storage

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,83 +1,88 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"math/rand"
+	"context"
 	"encoding/json"
-)
+	"fmt"
+	"math/rand"
+	"net/http"
 
-var urlStore = make(map[string]string)
+	"github.com/zhenghchen/mini-url-shortener/pkg/database"
+)
 
 const charset = "abcdefghijklmnopqrstuvwxyz0123456789"
 
 func generateCode() string {
+
 	code := make([]byte, 6)
 
 	for i := range code {
-
 		code[i] = charset[rand.Intn(len(charset))]
-
 	}
-	
+
 	return string(code)
 }
 
-
 func main() {
-	
-	http.HandleFunc("/", handleRedirect)
-	http.HandleFunc("/shorten", handleShorten) 
+	ctx := context.Background()
+
+	store, err := database.NewStore(ctx, "http://localhost:8000")
+
+	if err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/shorten", func(w http.ResponseWriter, r *http.Request) {
+		handleShorten(w, r, store)
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handleRedirect(w, r, store)
+	})
 
 	fmt.Println("Server running on :8080")
 	http.ListenAndServe(":8080", nil)
-	
+
+
+
 }
 
-func handleShorten(w http.ResponseWriter, r *http.Request) {
-	
+func handleShorten(w http.ResponseWriter, r *http.Request, store *database.Store) {
+
 	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
 	var body struct {
-		URL string `json:url`
+		URL string `json:"url"`
 	}
 
 	if err := json.NewDecoder(r.Body).Decode(&body); err != nil || body.URL == "" {
-	
 		http.Error(w, "invalid request body", http.StatusBadRequest)
 		return
-
 	}
 
 	code := generateCode()
-	urlStore[code] = body.URL
-	
 
-	// this adds the header to the response saying that its a json object
+	if err := store.SaveURL(r.Context(), code, body.URL); err != nil {
+		fmt.Println("save error:", err)
+		http.Error(w, "failed to save", http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	
-	// new encoder streams json into the response, encode turns the map that we need
-	// to return to the redirect into json. 
-	json.NewEncoder(w).Encode(map[string]string{"code":code})
+	json.NewEncoder(w).Encode(map[string]string{"code": code})
 
 
-	
 }
 
-func handleRedirect(w http.ResponseWriter, r *http.Request) {
-	// need to strip the first /
+func handleRedirect(w http.ResponseWriter, r *http.Request, store *database.Store) {
 	code := r.URL.Path[1:]
-	url, ok := urlStore[code]
-	
-	if !ok {
-
+	url, err := store.GetURL(r.Context(), code)
+	if err != nil || url == "" {
 		http.Error(w, "not found", http.StatusNotFound)
 		return
-
 	}
 	http.Redirect(w, r, url, http.StatusFound)
-
 }
---
+++ pkg/database/dynamo.go
@@ -0,0 +1,85 @@
+package database
+
+import (
+	"context"
+	"github.com/aws/aws-sdk-go-v2/aws"
+    "github.com/aws/aws-sdk-go-v2/config"
+    "github.com/aws/aws-sdk-go-v2/credentials"
+    "github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+    "github.com/aws/aws-sdk-go-v2/service/dynamodb"
+    "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+)
+
+const tableName = "urls"
+
+type Store struct {
+	client *dynamodb.Client
+}
+
+func NewStore(ctx context.Context, endpoint string) (*Store, error) {
+
+	cfg, err := config.LoadDefaultConfig(ctx,
+		config.WithRegion("us-east-1"),
+    	config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider("fake", "fake", "")),
+    )
+    if err != nil {
+        return nil, err
+    }
+
+	client := dynamodb.NewFromConfig(cfg, func(o *dynamodb.Options) {
+		o.BaseEndpoint = aws.String(endpoint)
+
+	})
+
+	return &Store{client: client}, nil
+
+}
+
+func (s *Store) SaveURL(ctx context.Context, code, url string) error {
+	item, err := attributevalue.MarshalMap(map[string]string{
+		"code": code,
+		"url":  url,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	_, err = s.client.PutItem(ctx, &dynamodb.PutItemInput {
+			TableName: aws.String(tableName),
+			Item: item,
+		})
+
+	return err
+}
+
+func (s *Store) GetURL(ctx context.Context, code string) (string, error) {
+	result, err := s.client.GetItem(ctx, &dynamodb.GetItemInput{
+		TableName: aws.String(tableName),
+
+		// The key is matchign primary key to map with { "code" : Value}
+		// this is needed because dynamo is stored with explicit type information
+		// "code": {"S": "abc123"}
+		Key: map[string]types.AttributeValue{
+			"code": &types.AttributeValueMemberS{Value: code},
+		}, 
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	if result.Item == nil {
+		return "", nil
+	}
+
+	var record map[string]string
+	if err := attributevalue.UnmarshalMap(result.Item, &record); err != nil {
+		return "", err
+	}
+
+	return record["url"], nil
+	
+
+
+}
